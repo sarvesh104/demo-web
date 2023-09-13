5b980b6ccbc07fb0252e91e95fa59c00b3e49866
sarvesh104 <sarveshdusane104gmail.com>
2023-09-14 01:04:07 +0530

    firstintro page done

---
+++ web-demo/src/Layout/Layout.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from '../components/Navbar';
+import Intro from '../components/Intro';
+import About from '../components/About';
+import Answer from '../components/Answer';
+
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />
+      <br/>
+      <Intro />
+      <About />
+      <Answer />
+    </div>
+  )
+}
+
+export default Layout;
---
+++ web-demo/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Logo from "../images/passopen.png";
+
+const Navbar = () => {
+  return (
+    <div className="main-nav">
+    <div className="nav-parts">
+      <div className="logo"><img src={Logo} style={{    width: "165px",
+    height: "45px"}}/></div>
+      <div className="right-but">
+        <div className="login">Login</div>
+        <div className="get-app">Get <span> App</span></div>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Navbar;
